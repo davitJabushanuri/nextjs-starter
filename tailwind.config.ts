34483c8d89345ec6768a0cc899e0de3ca33ef3aa
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2025-06-18 00:10:14 +0400

    chore: update configuration files for improved project structure and consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-	theme: {
-		extend: {
-			backgroundImage: {},
-		},
-	},
-	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundImage: {},
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
