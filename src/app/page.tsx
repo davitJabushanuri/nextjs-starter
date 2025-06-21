6551ca54d1de5760ec9069defa959ea1973d8d75
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2025-06-22 00:57:23 +0400

    chore: implement user fetching with error boundary and suspense for improved loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
-export default function Home() {
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { API_URL } from "@/config";
+import { logger } from "@/utils/logger";
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
+export default async function Home() {
   return (
     <div>
       <h1 className="">Nextjs starter</h1>
+      <ErrorBoundary fallback={<div>hello</div>}>
+        <Suspense fallback={<div>Loading user...</div>}>
+          <User />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
+
+async function User() {
+  const user = await getUser();
+
+  return (
+    <div className="user-info">
+      <h2 className="font-bold text-2xl">User Information</h2>
+      <p className="text-lg">Name: {user.firstName}</p>
+      <p className="text-lg">Last Name: {user.lastName}</p>
+      <p>{Math.random()}</p>
+    </div>
+  );
+}
+
+const getUser = async (): Promise<User> => {
+  try {
+    const response = await fetch(`${API_URL}/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching user: ${response.statusText}`);
+    }
+
+    return (await response.json()) as User;
+  } catch (error) {
+    logger.error("Failed to fetch user:", error);
+    throw error;
+  }
+};
