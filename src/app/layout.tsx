887800eac20c8489580c69fce165a83a843a53bd
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2025-06-22 00:56:23 +0400

    chore: add environment configuration, mock server setup, and update layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,29 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import { MOCKS_ENABLED, NEXT_RUNTIME } from "@/config";
+import { AppProviders } from "@/providers";
 
 export const metadata: Metadata = {
   title: "Nextjs starter",
   description: "A Next.js starter template",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (NEXT_RUNTIME === "nodejs" && MOCKS_ENABLED === "true") {
+    const { server } = await import("@/mocks/server");
+    server.listen({ onUnhandledRequest: "error" });
+  }
+
   return (
     <html lang="en" className="h-full">
       <body className="h-full bg-background-100 text-foreground-100">
-        <main className="grid h-full">{children}</main>
+        <AppProviders>
+          <main className="grid h-full">{children}</main>
+        </AppProviders>
       </body>
     </html>
   );
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const MOCKS_ENABLED = process.env.MOCKS_ENABLED;
+export const NEXT_RUNTIME = process.env.NEXT_RUNTIME;
